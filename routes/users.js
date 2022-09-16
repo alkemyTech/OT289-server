22ef5af16425f51caee58c5b4d781f935aa61e64
danielDeVita <danielitodevita@gmail.com>
2022-09-16 14:46:25 -0300

    login authentication and validation

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const userControllers = require('../controllers/userControllers');
+const validator = require('../middlewares/userLoginExpressValidator');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
+/* POST route to authenticate user to login */
+router.post('/auth/login', validator, userControllers.loginProcess);
 
 module.exports = router;
