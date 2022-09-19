8b5da07d909b71108c8e2f51e720a5b129147f0e
gervy gonzalez <gervygonzalez@gmail.com>
2022-09-19 14:38:05 -0300

    endpoint register of user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,56 @@
 var express = require('express');
 var router = express.Router();
+const { body, validationResult } = require('express-validator');
+const db = require('../models/index'); 
+const bcrypt = require('bcrypt')
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+
+router.post('/auth/register',
+  body('username').isLength({min:2}).withMessage('El nombre es requerido y debe tener mas de dos caracteres'),
+  body('lastname').isLength({min:2}).withMessage('El apellido es requerido y debe tener mas de dos caracteres'),  
+  body('email').isEmail().withMessage('El formato de Email debe ser válido'),
+   // password must be at least 6chars long
+  body('password').isLength({ min: 6 }).withMessage('El password debe tener minimo 6 caracteres'),
+  async (req, res) => {
+     
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    
+    const { username, lastname, email, password } = req.body; 
+    if (await db.User.findOne({ where: { email: email } })) {
+      return res.status(500).json('El usuario ingresado ya existe');
+    }
+    const salt= await bcrypt.genSalt(10);
+    const passHash = await bcrypt.hash(password, salt );    
+
+    const objUser= {
+      firstName: username,
+      lastName: lastname,
+      email: email,
+      password: passHash
+    }
+    const user = new db.User(objUser);    
+
+    return res.json( await user.save() );
+
+    
+  },
+);
+
+
+
+
+
+
+
+
 
 module.exports = router;
