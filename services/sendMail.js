fbe83ab5270e3b05573413b0c411216147e960ad
Ignamaldonado <ignacio.maldonado96@gmail.com>
2022-09-21 20:01:48 -0300

    created the service

---
+++ services/sendMail.js
@@ -0,0 +1,27 @@
+export async function sendMail(to, subject, text, html) {
+   const config = {
+     host: 'smtp.gmail.com',
+     port: '587',
+     auth: {
+       user: 'somosmas2022ong@gmail.com',
+       pass: process.env.MAILER_PASSWORD
+     }
+   }
+
+   const transport = nodemailer.createTransport(config)
+
+   const msg = {
+    to,
+    from:'somosmas2022ong@gmail.com',
+    subject,
+    text,
+    html,
+   }
+
+   try {
+    const result = await transport.sendMail(msg)
+    return result
+  } catch (error) {
+    res.send(error.message)
+  }
+}
