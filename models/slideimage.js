3bd4221870f5a1b16ee2d024cc71798a88dcb9ac
Ignamaldonado <ignacio.maldonado96@gmail.com>
2022-09-24 09:29:17 -0300

    created the migration and the model from console

--- models/slideimage.js
+++ models/slideimage.js
@@ -1,37 +1,27 @@
-module.exports = (sequelize, dataTypes) => {
-    const alias = 'SlideImage'
-    const cols = {
-        id: {
-            type: dataTypes.INTEGER,
-            primaryKey: true,
-            allowNull: false,
-            autoIncrement: true
-        },
-        imageUrl: {
-            type: dataTypes.STRING,
-            allowNull: false
-        },
-        text: {
-            type: dataTypes.STRING,
-            allowNull: false
-        },
-        order: {
-            type: dataTypes.INTEGER,
-            allowNull: false
-        },
-        organizationId: {
-            type: dataTypes.INTEGER,
-        },
-        deletedAt: {
-            type: dataTypes.DATE
-        }
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class slideImage extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
     }
-    const config = {
-        tableName: 'slideImages',
-        timestamps: true
-    }
-
-    const SlideImage = sequelize.define(alias,cols,config)
-
-    return SlideImage
-}
+  };
+  slideImage.init({
+    id: DataTypes.INTEGER,
+    imageUrl: DataTypes.STRING,
+    text: DataTypes.STRING,
+    order: DataTypes.INTEGER,
+    organizationId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'slideImage',
+  });
+  return slideImage;
+};
