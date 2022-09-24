bfcec58f439621370dc4eb795bc51aa0753dd248
Ignamaldonado <ignacio.maldonado96@gmail.com>
2022-09-24 09:31:33 -0300

    fixed capital letter from the model

--- models/slideimage.js
+++ models/slideimage.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class slideImage extends Model {
+  class SlideImage extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,7 +13,7 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   };
-  slideImage.init({
+  SlideImage.init({
     id: DataTypes.INTEGER,
     imageUrl: DataTypes.STRING,
     text: DataTypes.STRING,
