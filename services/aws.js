9e16f93bb51e5ee43e0aac7e21363f612c51582b
Facundo Azcue <fazcue@gmail.com>
2022-09-26 22:01:00 -0300

    Add 'uploadFile' & 'deleteFile' fn + Add express-fileupload for future use

--- services/aws.js
+++ services/aws.js
@@ -1,19 +1,38 @@
-const { S3Client } = require("@aws-sdk/client-s3")
+const { S3Client, PutObjectCommand, DeleteObjectCommand } = require("@aws-sdk/client-s3")
 
 const AWS_BUCKET_REGION = process.env.AWS_BUCKET_REGION
+const AWS_BUCKET_NAME = process.env.AWS_BUCKET_NAME
 const AWS_PUBLIC_KEY = process.env.AWS_PUBLIC_KEY
 const AWS_SECRET_KEY = process.env.AWS_SECRET_KEY
 
-//If more aws services are installed, add them here
-//Example how to use: awsService.s3Client.send()
-const awsService = {
-    s3Client: new S3Client({ 
-        region: AWS_BUCKET_REGION,
-        credentials: {
-            accessKeyId: AWS_PUBLIC_KEY,
-            secretAccessKey: AWS_SECRET_KEY
+const s3 = new S3Client({ 
+    region: AWS_BUCKET_REGION,
+    credentials: {
+        accessKeyId: AWS_PUBLIC_KEY,
+        secretAccessKey: AWS_SECRET_KEY
+    }
+})
+
+//To get file url:
+//https://BUCKETNAME.s3.amazonaws.com/KEY
+const aws = {
+    uploadFile: async (fileName, fileData) => {
+        const uploadParams = {
+            Bucket: AWS_BUCKET_NAME,
+            Key: fileName,
+            Body: fileData
         }
-    })
+        const command = new PutObjectCommand(uploadParams)
+        await s3.send(command)
+    },
+    deleteFile: async (fileName) => {
+        const deleteParams = {
+            Bucket: AWS_BUCKET_NAME,
+            Key: fileName
+        }
+        const command = new DeleteObjectCommand(deleteParams)
+        await s3.send(command)
+    }
 }
 
-module.exports = awsService
+module.exports = aws
