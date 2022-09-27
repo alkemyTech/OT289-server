a126d0d15542b2f5029b991e5ea9ee45566a919f
Ignamaldonado <ignacio.maldonado96@gmail.com>
2022-09-27 20:34:58 -0300

    finished the controller

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -2,11 +2,46 @@ const db = require('../models')
 
 const newsController = {
     update: (req,res) => {
+        const id = req.params.id
+        const name = req.body.name
+        const image = req.body.image
+        const categoryId = req.body.categoryId
+        const type = req.body.type
+
         db.news.update({
-            
+            id,
+            name,
+            image,
+            categoryId,
+            type
+
         },{
-            where: {id: req.params.id}
+            where: {id}
         })
+            .then(( confirm => {
+                let answer;
+                if(confirm) {
+                    answer = {
+                        meta: {
+                            status: 200,
+                            total: confirm.length,
+                            url: `/news/${id}`
+                        },
+                        data:confirm
+                    }
+                } else {
+                    answer = {
+                        meta: {
+                            status: 204, 
+                            total: confirm.length,
+                            url: `/news/${id}`
+                        },
+                        data: confirm
+                    }
+                }
+                res.json(answer)
+            }))
+            .catch(error => res.send(error))
     }
 }
 
