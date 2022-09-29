32336830da104169ba6f9a98c9b5d3958c9b2dcd
Gabriel <ahrensgabriel09@gmail.com>
2022-09-29 14:50:50 -0300

    Add getNews to newsControllers object

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -32,24 +32,20 @@ const newsControllers = {
 
         const newEntry = new db.Entries(entryObj)
         return res.json( await newEntry.save() )
+    },
+    getNews: async (req, res) => {
+      try {
+        const news = await db.Entries.findAll({
+          where: {
+            type: 'news'
+          },
+          attributes: ['name', 'image', 'createdAt']
+        })
+        news.length? res.status(200).send(news) : res.status(200).send('Not found') 
+      } catch (error) {
+        res.status(400).send(error.message)
+      }
     }
 }
 
-const getNews = async (req, res) => {
-  try {
-    const news = await db.Entries.findAll({
-      where: {
-        type: 'news'
-      },
-      attributes: ['name', 'image', 'createdAt']
-    })
-    news.length? res.status(200).send(news) : res.status(200).send('Not found') 
-  } catch (error) {
-    res.status(400).send(error.message)
-  }
-}
-
-module.exports = {
-  newsControllers,
-  getNews,
-}
+module.exports = newsControllers
--- routes/news.js
+++ routes/news.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-const { newsControllers, getNews } = require('../controllers/newsController.js')
+const newsControllers = require('../controllers/newsController.js')
 const newsValidator = require('../middlewares/newsValidator')
 
 //POST add new entry to "Entries" with type "news"
 router.post('/', newsValidator, newsControllers.add)
 
 //GET return the list of entries where the type field is "news"
-router.get('/', getNews)
+router.get('/', newsControllers.getNews)
 
 module.exports = router
