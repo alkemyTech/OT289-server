f91693c26da5825e6d55084ffa9cfa84884c6d47
Gabriel <ahrensgabriel09@gmail.com>
2022-09-29 14:04:42 -0300

    Fix import problem

--- routes/news.js
+++ routes/news.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-const { newsController, getNews } = require('../controllers/newsController.js')
+const { newsControllers, getNews } = require('../controllers/newsController.js')
 const newsValidator = require('../middlewares/newsValidator')
 
 //POST add new entry to "Entries" with type "news"
-router.post('/', newsValidator, newsController.add)
+router.post('/', newsValidator, newsControllers.add)
 
 //GET return the list of entries where the type field is "news"
 router.get('/', getNews)
