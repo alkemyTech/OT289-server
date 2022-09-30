8d3fe2d40a95f16b56079e93ae6482ef776567b7
danielDeVita <danielitodevita@gmail.com>
2022-09-30 10:51:10 -0300

    creation of a validator file for the testimonials, added endpoint/route to post testimonials and implemented controller with messages for every scenario

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -1,17 +1,36 @@
 let mockDataJson = require('./MOCK_DATA.json')
 let mockData = JSON.parse(JSON.stringify(mockDataJson))
+const db = require('../models');
+const { validationResult } = require("express-validator");
 
 
 const mainControllers = {
-    index:(req, res)=>{
+    index: (req, res) => {
         res.render('index', { title: 'Express' })
     },
-    public:(req, res)=>{
-        let response =  mockData.find((item)=>{return item.id == req.params.id})
-        if (response){
+    public: (req, res) => {
+        let response = mockData.find((item) => { return item.id == req.params.id })
+        if (response) {
             return res.status(200).json(response)
-        }else{
-            return res.status(400).json({errors:[{msg:"No organization found"}]})
+        } else {
+            return res.status(400).json({ errors: [{ msg: "No organization found" }] })
+        }
+    },
+    testimonials: (req, res) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.json({errors: errors.mapped()});
+        } else {
+            db.Testimonials.create({
+                name: req.body.name,
+                content: req.body.content
+            })
+                .then((testimony) => {
+                    return res.status(200).send(testimony)
+                })
+                .catch(error => {
+                    console.error(error)
+                })
         }
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 let express = require('express');
 let router = express.Router();
 let mainControllers = require("../controllers/mainControllers")
+let testimonialsValidator = require("../middlewares/testimonialsValidator");
+
+
 /* GET home page. */
 router.get('/', mainControllers.index);
+
+/* POST ROUTE FOR TESTIMONIALS */
+router.post('/testimonials', testimonialsValidator,mainControllers.testimonials);
+
 router.get('/organizations/:id/public', mainControllers.public)
 
 module.exports = router;
