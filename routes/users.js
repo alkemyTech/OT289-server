080f048f14ffe601654c5eb82ffdbcfbbf4499cd
gervy gonzalez <gervygonzalez@gmail.com>
2022-10-03 22:27:14 -0300

    fature/OT289-54 conflict resolution for order code

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const userControllers = require('../controllers/userControllers');
 const validator = require('../middlewares/expressValidator');
 
-const { loginProcess } = require('../controllers/userControllers');
-
+//GET list of all users.
+router.get('/', userController.listAllUsers);
 
 /* POST route to authenticate user to login */
 router.post('/auth/login', validator.login , userController.login);
 router.post('/auth/register', validator.register ,userController.register);
-
-//GET list of all users.
-router.get('/', userController.listAllUsers);
 
 router.post('/auth/checkEmail' , userController.checkEmail);
 router.post('/auth/checkPassword' , userController.checkPassword);
