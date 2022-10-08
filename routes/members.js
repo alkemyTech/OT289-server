da1d8b7dfdd7d1208f11d9a0d79cda068d1a349d
gervy gonzalez <gervygonzalez@gmail.com>
2022-10-08 01:22:03 -0300

    feature/OT289-91 endpoint create members

--- routes/members.js
+++ routes/members.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const membersController = require('../controllers/membersController.js')
-
+const membersValidator = require('../middlewares/membersValidator')
 //DELETE member by :id
 router.delete("/:id", membersController.destroy)
 
+//POST member by name
+router.post("/", membersValidator, membersController.createMember)
 module.exports = router
