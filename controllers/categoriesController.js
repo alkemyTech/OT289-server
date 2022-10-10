347661cab84964ba7f110556af804baecaf8d9b3
danielDeVita <danielitodevita@gmail.com>
2022-10-10 19:31:06 -0300

    endpoint to post categories implemented

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -3,39 +3,56 @@ const { validationResult } = require("express-validator");
 
 const testimonialsController = {
     deleteCategory: (req, res) => {
-        db.Categories.destroy({where:{id: req.params.id}})
-        .then((category) => {
-            if(category[0] === 0){
-                return res.status(400).json({errors:[{msg:"No pudimos encontrar este testimonio"}]})
-            }
-            return res.status(200).json({category})
-        })
-        .catch(error => {
-            console.error(error)
-            return res.status(400).json({errors:[{msg:"Estamos teniendo problemas en nuestras bases de datos, por favor intente mas tarde"}]})
-        })
+        db.Categories.destroy({ where: { id: req.params.id } })
+            .then((category) => {
+                if (category[0] === 0) {
+                    return res.status(400).json({ errors: [{ msg: "No pudimos encontrar este testimonio" }] })
+                }
+                return res.status(200).json({ category })
+            })
+            .catch(error => {
+                console.error(error)
+                return res.status(400).json({ errors: [{ msg: "Estamos teniendo problemas en nuestras bases de datos, por favor intente mas tarde" }] })
+            })
     },
     getCategories: (req, res) => {
         db.Categories.findAll()
             .then(data => {
-                const newData = data.map(category => {return {
-                    id: category.dataValues.id,
-                    name: category.dataValues.name
-                }})
+                const newData = data.map(category => {
+                    return {
+                        id: category.dataValues.id,
+                        name: category.dataValues.name
+                    }
+                })
                 return res.status(200).send(newData)
             })
-                .catch(error => {
-                    const errorObj = {
-                        error: 'Problemas en la base de datos, por favor intente mas tarde', 
-                        sequelizeError: {
-                            code: error.parent.code,
-                            nro:error.parent.errno
-                        }
+            .catch(error => {
+                const errorObj = {
+                    error: 'Problemas en la base de datos, por favor intente mas tarde',
+                    sequelizeError: {
+                        code: error.parent.code,
+                        nro: error.parent.errno
                     }
-                    return res.status(400).json(errorObj)
+                }
+                return res.status(400).json(errorObj)
+            })
+    },
+    postCategory: (req, res) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.json({ errors: errors.mapped() });
+        } else {
+            db.Categories.create({
+                name: req.body.name,
+            })
+                .then((categorie) => {
+                    return res.status(200).json({ message: "Categorie created succesfully", categorie })
                 })
-    }
-
+                .catch(error => {
+                    console.error(error)
+                })
+        }
+    },
 }
 
 module.exports = testimonialsController;
--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const categoriesController = require("../controllers/categoriesController");
+const categoriesValidator = require("../middlewares/categoriesValidator");
+
+router.post("/", categoriesValidator, categoriesController.postCategory);
 
 router.delete('/delete/:id', categoriesController.deleteCategory);
 router.get('/', categoriesController.getCategories)
