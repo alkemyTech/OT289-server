b56918e70c2d45482e02ce873ed75ee3be2da004
Ignamaldonado <ignacio.maldonado96@gmail.com>
2022-10-16 18:31:12 -0300

    finished the testing!!

--- controllers/membersController.js
+++ controllers/membersController.js
@@ -8,12 +8,12 @@ const membersController = {
             const deleted = await db.Members.destroy({ where: { id } })
 
             if (deleted) {
-                res.status(200).send('Miembro eliminado')
+                res.status(200).json({success: 'Miembro eliminado'})
             } else {
-                res.status(200).json({ error: 'Miembro inexistente' })
+                res.status(404).json({ error: 'Miembro inexistente' })
             }
         } catch (error) {
-            res.status(400).json(error)
+            res.status(503).json(error)
         }
     },
 
@@ -23,57 +23,97 @@ const membersController = {
           const allMembers = await db.Members.findAll();
           allMembers.length > 0
             ? res.status(200).json(allMembers)
-            : res.status(200).send("No se encontraron Registros");
+            : res.status(404).send("No se encontraron Registros");
         } catch (error) {
-          res.status(500).json(error.message);
+          res.status(503).json(error.message);
         }
     },
 
     createMember: async (req, res) => {
-          
+           
+            const { name, image } = req.body; 
+            
             const errors = validationResult(req);
             if (!errors.isEmpty()) {
               return res.status(400).json({ errors: errors.array() });
             }
-           
-            const { name, image } = req.body; 
-        
+
             const entryObj = {
               name,
               image            
             };
         
             const newEntry = new db.Members(entryObj);
-            return res.json(await newEntry.save());  
+            return res.status(200).json(await newEntry.save());  
   
   },      
   
 
     update: async (req, res) => {
+        const id = req.params.id;
+        let { name, image } = req.body;
+
+        console.log(name)
+
+        const oldData = await db.Members.findByPk(id)
+
+        if(oldData === null) {
+            return res.status(404).json({errors: 'Not found'})
+        }
+
+        const nameValidation = /^$|^[A-Za-z\s]+$/.test(name)
+        const imageValidation = /^$|\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(image)
+
+        console.log(nameValidation)
+
+        if(!nameValidation) {
+            return res.status(400).json({errors: 'Invalid name'})
+        }
+
+        if(!imageValidation) {
+            return res.status(400).json({errors: 'Invalid image'})
+        }
+        
+
+        if (name === '' && image === '') {
+            return res.status(400).json({errors: 'Bad request'})
+        }
+        
+        if(name === undefined || image === undefined) {
+            return res.status(400).json({errors: 'Bad request'})
+        }
+
+
+        if(name === '') {
+            name = oldData.dataValues.name
+        }
+
+        if (image === '') {
+            image = oldData.dataValues.image
+        }
+
+
+        const member = {
+            name,
+            image
+        }
+
         try {
-            let updated = await db.Members.update({
-                name: req.body.name,
-                image: req.body.image
-            },
+            let updated = await db.Members.update(member,
                 {
                     where: { id: req.params.id }
                 }
             );
             if (updated) {
-                res.status(200).send("Información actualizada")
+                res.status(200).json(member)
             } else {
-                res.status(503).send("No se ha podido actualizar la información")
+                res.status(400).json({errors: "No se ha podido actualizar la información"})
             };
         }
         catch (error) {
-            console.error(error)
+            res.status(503).json(error)
         }
     },
-    getMembers: (req, res) => {
-        db.Members.findAll()
-            .then(data => res.status(200).json(data))
-                .catch(error => res.status(400).send('Error en la base de datos'))
-    }
 }
 
     
