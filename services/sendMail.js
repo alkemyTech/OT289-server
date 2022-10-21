69db12644c100bb3670ecdd7cdb594d01bae7b90
gervy gonzalez <gervygonzalez@gmail.com>
2022-10-21 00:17:58 -0300

    feature/OT289-107-b send mail contact

--- services/sendMail.js
+++ services/sendMail.js
@@ -1,13 +1,13 @@
 const nodemailer = require('nodemailer')
 
 async function sendMail(to, subject, text, html) {
-  const PASS_MAIL = process.env.MAILER_PASSWORD; console.log(PASS_MAIL)
+  
    const config = { 
      host: 'smtp.gmail.com',
      port: '587',
      auth: {
        user: 'somosmas2022ong@gmail.com',
-       pass: PASS_MAIL
+       pass: process.env.MAILER_PASSWORD
      }
    }
 
